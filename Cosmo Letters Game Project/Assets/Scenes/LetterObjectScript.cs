f97e9c8c808a92c03594d48f37041ef4d86607c0
Himani-K123 <dr.himanipkumar@gmail.com>
2023-06-05 21:50:53 -0400

    Initial Work on Buttons for Input on Scrabble Screen

---
+++ Cosmo Letters Game Project/Assets/Scenes/LetterObjectScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LetterObject : MonoBehaviour
+{
+    public char[] letters = { 'A', 'B', 'C', 'D', 'E' }; // Array of letters
+
+    public void RandomizeLetterAndLocation()
+    {
+        Debug.Log("Randomize"); 
+        int randomIndex = Random.Range(0, letters.Length);
+        char randomLetter = letters[randomIndex];
+    
+        // Update the letter object with the new letter
+        TextMesh textMesh = GetComponent<TextMesh>();
+        textMesh.text = randomLetter.ToString();
+    }
+
+}
